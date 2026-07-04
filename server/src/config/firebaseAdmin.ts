4018a6da37ec8daf55507ce340e4ff0a2f172286
Tamilselvan <tamilselvan0298@gmail.com>
2026-07-04 19:40:18 +0530

    Sanitize private key escapes in firebaseAdmin config

--- server/src/config/firebaseAdmin.ts
+++ server/src/config/firebaseAdmin.ts
@@ -9,6 +9,9 @@ try {
   // If FIREBASE_SERVICE_ACCOUNT_JSON is set, parse and initialize
   if (process.env.FIREBASE_SERVICE_ACCOUNT_JSON && !process.env.FIREBASE_SERVICE_ACCOUNT_JSON.startsWith('PLACEHOLDER')) {
     const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+    if (serviceAccount.private_key) {
+      serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+    }
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
